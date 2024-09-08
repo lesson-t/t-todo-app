f3c01eeb919420e949f95ebb65fa329ec5b05e17
takeshi <goozooloo123@gmail.com>
2024-09-08 12:15:14 +0900

    board routesの作成, boardモデルに各簡易コントローラ追加, viewの追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,5 +4,36 @@ class BoardsController < ApplicationController
         # @boards = Boards.all
     end
 
+    def show
+    end
+
+    def new
+        @board = current_user.boards.build
+    end
+
+    def create
+        @board = current_user.boards.build(board_params)
+        if @board.save
+            redirect_to board_path(@board), notice: '保存できました'
+        else
+            flash.now[:error] = '保存に失敗しました'
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def board_params
+        params.require(:board).permit(:title, :content)
+    end
+
 
 end
