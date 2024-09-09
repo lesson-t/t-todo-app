7143bcacf3e1b588f5f19c0a222b4beb6f767415
takeshi <goozooloo123@gmail.com>
2024-09-09 17:00:20 +0900

    profile のモデル関連付け, routesの設定, controller show / edit / update の作成, view の作成

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,25 @@
+class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+        @profile = current_user.profile
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :birthday,
+            :subscribe
+            # ,:avatar
+        )
+    end
+end
