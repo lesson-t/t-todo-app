edd7f7d9cf45b65e4d7258dc20ecd5711bc28a0c
takeshi <goozooloo123@gmail.com>
2024-09-10 07:42:39 +0900

    profile controller update の実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,14 @@ class ProfilesController < ApplicationController
     end
 
     def update
+        @profile = current_user.profile || current_user.build_profile
+        @profile.assign_attributes(profile_params)
+        if @profile.save
+            redirect_to profile_path, notice: 'プロフィールを更新'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
     end
 
     private
