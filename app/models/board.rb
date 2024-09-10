cc59a4050f86ed8b1f6d77632b4dfe9ca2b38994
takeshi <goozooloo123@gmail.com>
2024-09-10 11:36:37 +0900

    avatar を表示できるよう修正, ヘッダのメニュの修正

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,4 +3,12 @@ class Board < ApplicationRecord
     validates :content, presence: true
 
     belongs_to :user
+
+    def display_created_at
+        I18n.l(self.created_at, format: :default)
+    end
+
+    def author_name
+        user.display_name
+    end
 end
