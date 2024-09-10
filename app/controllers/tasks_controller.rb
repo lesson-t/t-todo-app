713a8a72f16a06ba696dd67fa8291e71743c9122
takeshi <goozooloo123@gmail.com>
2024-09-11 07:07:26 +0900

    controller new / create の実装, board からのリンク設置

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,20 @@ class TasksController < ApplicationController
     before_action :authenticate_user!
 
     def new
+        board = Board.find(params[:board_id])
+        @task = board.tasks.build
     end
 
     def create
+        board = Board.find(params[:board_id])
+        @task = board.tasks.build(task_params)
+        @task.user = current_user
+        if @task.save
+            redirect_to boards_path(board), notice: 'add Task'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :new
+        end
     end
 
     def edit
@@ -18,5 +29,11 @@ class TasksController < ApplicationController
 
     private
     def task_params
+        params.require(:task).permit(
+            :title,
+            :content,
+            :deadline,
+            :eyecatch
+        )
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
     belongs_to :user
     belongs_to :board
+
+    has_one_attached :eyecatch
 end
