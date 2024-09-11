27f3c32caee074f3b4c09827c32620e00b2aef44
takeshi <goozooloo123@gmail.com>
2024-09-12 06:18:23 +0900

    routes comments 設定, cotroller new / create 設定, view new 設定, モデル関連付け

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+    def new
+    end
+
+    def create
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
     belongs_to :user
     belongs_to :board
 
+    has_many :comments, dependent: :destroy
+
     has_one_attached :eyecatch
 
     def display_created_at
