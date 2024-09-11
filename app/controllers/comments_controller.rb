90339a74a9b56305446a256feec35c1a196014a6
takeshi <goozooloo123@gmail.com>
2024-09-12 06:54:23 +0900

    controller new / create 実装, new フォーム作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,19 @@
 class CommentsController < ApplicationController
     def new
+        task = Task.find(params[:task_id])
+        @comment = task.comments.build
     end
 
     def create
+        task = Task.find(params[:task_id])
+        @comment = task.comments.build(comment_params)
+        @comment.user = current_user
+        if @comment.save
+            redirect_to board_task_path(task.board, task.id), notice: 'Comment Add'
+        else
+            flash.now[:error] = 'Update Failed'
+            render :new
+        end
     end
 
     private
